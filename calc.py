1663b0baea50cb2a7a986924aaccf5cfc192b720
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 09:39:09 +0700

    added operator buttons

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-root =Tk()
+root=Tk()
 display = Entry(root)
 display.grid(row=1, columnspan=6)
 
@@ -13,5 +13,14 @@ for x in range(3):
     
 button = Button(root, text="0", width=2, height=2)
 button.grid(row=5, column=1)
+
+count = 0
+operations = ["+","-","*","/","*3.14","%","(","**",")","**2"]
+for x in range(4):
+  for y in range(3):
+    if count < len(operations):
+      button = Button(root,text=operations[count],width=2,height=2)
+      count += 1
+      button.grid(row=x+2,column=y+3) 
 
 root.mainloop()
