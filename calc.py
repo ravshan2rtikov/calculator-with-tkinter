496a94190ce53b7a924a23d81c40ef6afad19f70
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 10:10:59 +0700

    made the buttons insert value into display

--- calc.py
+++ calc.py
@@ -1,17 +1,28 @@
 from tkinter import *
 
 root=Tk()
+
+i = 0
+def get_number(num):
+  global i
+  display.insert(i,num)
+  i+=1
+
+
 display = Entry(root)
 display.grid(row=1, columnspan=6)
 
-counter = 1
+
+numbers = [1,2,3,4,5,6,7,8,9]
+counter = 0
 for x in range(3):
   for y in range(3):
-    button = Button(root, text=counter, width=2, height=2)
+    button_text = numbers[counter]
+    button = Button(root, text=button_text, width=2, height=2, command = lambda text=button_text:get_number(text))
     button.grid(row=x+2, column=y)
     counter += 1
     
-button = Button(root, text="0", width=2, height=2)
+button = Button(root, text="0", width=2, height=2, command=lambda :get_number(0))
 button.grid(row=5, column=1)
 
 count = 0
