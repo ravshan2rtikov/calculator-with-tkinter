87cf9e512bdeea1c93ca0da7fc8bd53592a82e88
ravshan2rtikov <ravshan2rtikov@gmail.com>
2025-03-06 10:33:34 +0700

    enabled AC button

--- calc.py
+++ calc.py
@@ -13,6 +13,9 @@ def get_operation(operator):
   length = len(operator)
   display.insert(i,operator)
   i+=length
+  
+def clear_all():
+  display.delete(0,END)
 
 display = Entry(root)
 display.grid(row=1, columnspan=6)
@@ -37,6 +40,8 @@ for x in range(4):
     if count < len(operations):
       button = Button(root,text=operations[count],width=2,height=2,command=lambda text=operations[count]:get_operation(text))
       count += 1
-      button.grid(row=x+2,column=y+3) 
-
+      button.grid(row=x+2,column=y+3)
+      
+Button(root,text="AC",width=2,height=2,command=clear_all).grid(row=5,column=0)
+Button(root,text="=",width=2,height=2).grid(row=5,column=2)
 root.mainloop()
